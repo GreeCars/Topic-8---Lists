76797656cb9e7c53aea929a43d0c04ddf4606114
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-10-10 09:39:01 -0400

    Working

--- Topic 8 - Lists/Program.cs
+++ Topic 8 - Lists/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+List<string> names = new List<string>{"Trinity", "Neo", "Morpheus", "Smith"};
+Console.WriteLine(names[1] + " is 'The One'.");
+Console.WriteLine(names[3] + " is the bad guy.");
+Console.WriteLine("There are " + names.Count + " names in my List.");
+names[3] = "Cypher";
+Console.WriteLine(names[names.Count - 1] + " is the bad guy.");
+Console.ReadLine(); // Keeps program from closing
